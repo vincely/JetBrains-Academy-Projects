5ddbd32213c782e7203546982a1c8c259bd028c9
vincely <vinzenz.ly@gmail.com>
2022-03-02 15:38:41 +0100

    added functionality to play the game. X is always starting
    finished game

--- Main.kt
+++ Main.kt
@@ -11,6 +11,7 @@ class TicTacToe {
         const val TOP_BOTTOM = "---------"
         const val LEFT = "| "
         const val RIGHT = " |"
+        const val GAME_INIT = "_________"
     }
 
     enum class States(val state: String) {
@@ -22,6 +23,7 @@ class TicTacToe {
     }
 
     val gameList2D = MutableList(3) { MutableList(3) { " " } }
+    var gameOver = false
 
     fun setGameState(state: String) {
 
@@ -37,19 +39,26 @@ class TicTacToe {
         val gameListAsString = gameList2D.flatten().joinToString("")
         val winnerList = checkWin(gameList2D)
         // check if one player has too many tokens (> 1)
-        if (abs(gameListAsString.count { it == PLAYER_O.first() } - gameListAsString.count { it == PLAYER_X.first() }) > 1) {
-        print(States.IMPOSSIBLE.state)
-        // check if there is more than 1 winner
-        } else if (winnerList[0] + winnerList[1] > 1) {
-            print(States.IMPOSSIBLE.state)
-        } else if (gameListAsString.contains('_') && (winnerList[0] + winnerList[1] == 0)) {
-            print(States.NOT_FINISHED.state)
-        } else if (!gameListAsString.contains('_') && (winnerList[0] + winnerList[1] == 0)) {
+        /* if (abs(gameListAsString.count { it == PLAYER_O.first() } - gameListAsString.count { it == PLAYER_X.first() }) > 1) {
+         print(States.IMPOSSIBLE.state)
+         // check if there is more than 1 winner
+         } else if (winnerList[0] + winnerList[1] > 1) {
+             print(States.IMPOSSIBLE.state)
+         } else if (gameListAsString.contains('_') && (winnerList[0] + winnerList[1] == 0)) {
+             print(States.NOT_FINISHED.state)
+         } else */
+        if (!gameListAsString.contains('_') && (winnerList[0] + winnerList[1] == 0)) {
             print(States.DRAW.state)
+            gameOver = true
+            return
         } else if (winnerList[0] == 1) {
             print(States.O_WINS.state)
-        } else {
+            gameOver = true
+            return
+        } else if (winnerList[1] == 1){
             print(States.X_WINS.state)
+            gameOver = true
+            return
         }
     }
 
@@ -86,11 +95,11 @@ class TicTacToe {
         for (row in gameList2D.indices) {
             sb.append(LEFT).append(gameList2D[row].joinToString(" ")).append(RIGHT).appendLine()
         }
-        sb.append(TOP_BOTTOM)
+        sb.append(TOP_BOTTOM).appendLine()
         return sb.toString()
     }
 
-    fun move() {
+    fun move(xHasTurn: Boolean) {
         print("Enter the coordinates: ")
         val coordinates = readln()
         val formatCheck = """\d\s*\d""".toRegex()
@@ -99,38 +108,48 @@ class TicTacToe {
             cordList.addAll(coordinates.split(" ").map { it.toInt() })
         } else {
             println("You should enter numbers!")
-            move()
+            move(xHasTurn)
             return
         }
 
         if (cordList.first() !in 1..3 || cordList.last() !in 1..3) {
             println("Coordinates should be from 1 to 3")
-            move()
+            move(xHasTurn)
             return
         } else if (gameList2D[cordList.first() - 1][cordList.last() - 1] != "_") {
             println("This cell is occupied! Choose another one!")
-            move()
+            move(xHasTurn)
             return
         }
-        gameList2D[cordList[0] - 1][cordList[1] - 1] = PLAYER_X
+        if (xHasTurn) {
+            gameList2D[cordList[0] - 1][cordList[1] - 1] = PLAYER_X
+        } else {
+            gameList2D[cordList[0] - 1][cordList[1] - 1] = PLAYER_O
+        }
         print(getGameString())
     }
 
+    fun play() {
+        var xHasTurn = true
+        while (!gameOver) {
+            move(xHasTurn)
+            evaluateState()
+            xHasTurn = !xHasTurn
+        }
+    }
 }
-
 
 
 
 fun main() {
     // write your code here
     val ticTacToe = TicTacToe()
-    print("Enter cells: ")
-    val input = readln()
-    ticTacToe.setGameState(input)
-    println(ticTacToe.getGameString())
+    // print("Enter cells: ")
+    // val input = readln()
+    ticTacToe.setGameState(TicTacToe.GAME_INIT)
+    print(ticTacToe.getGameString())
     //print(ticTacToe.gameList2D)
     // ticTacToe.evaluateState()
-    ticTacToe.move()
-
-
+    // ticTacToe.move()
+    ticTacToe.play()
 }
