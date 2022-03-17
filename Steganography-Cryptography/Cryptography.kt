35355baffadc3d79a65f750d4bcb304d1162a48d
vincely <vinzenz.ly@gmail.com>
2022-03-17 16:25:13 +0100

    finished cryptography project

--- Steganography-Cryptography/Cryptography.kt
+++ Steganography-Cryptography/Cryptography.kt
@@ -9,18 +9,19 @@ fun main() {
     ui.startMenu()
 
 }
+
 class ImageController {
 
-    var inputFile = ""
-    var secretMsg = ""
-    var outputFile = ""
+    private var inputFile = ""
+    private var secretMsg = ""
+    private var outputFile = ""
 
     /**
      * reads imageFile and catches io Exceptions.
      * returns a bufferedImage if it can be read.
      * otherwise, it will return null
      */
-    fun readImage(file: File): BufferedImage? {
+    private fun readImage(file: File): BufferedImage? {
         return try {
             ImageIO.read(file)
         } catch (e: Exception) {
@@ -28,21 +29,23 @@ class ImageController {
             null
         }
     }
+
     /**
      * saves the image file from the buffer in png
      * @param buffImage type BufferedImage
      * @param file
      *
      */
-    fun saveImageInPNG(buffImage: BufferedImage, file: File) {
+    private fun saveImageInPNG(buffImage: BufferedImage, file: File) {
         ImageIO.write(buffImage, "png", file)
         println("Message saved in ${file.path} image.")
     }
+
     /** Asks user to enter input name and output name.
      *  Then returns both names in a List<File> in the Format (input, output).
      *  The names are read via readln()
      * */
-    fun getImageConfig(): List<File> {
+    private fun getIOFilePaths(): List<File> {
         println("Input image file:")
         inputFile = readln()
         println("Output image file:")
@@ -54,10 +57,9 @@ class ImageController {
      * Asks for User Input to enter message to be hidden
      * function returns String containing the message
      */
-    fun getSecretMessage(): String {
+    private fun getSecretMessage(): String {
         println("Message to hide:")
         return readln()
-
     }
 
     /**
@@ -77,32 +79,18 @@ class ImageController {
         }
     }
 
-    fun printImageToBits(buffImage: BufferedImage) {
-        for (y in 0 until buffImage.height) {
-            for (x in 0 until buffImage.width) {
-                val color = Color(buffImage.getRGB(x, y))
-                val r = color.red
-                val g = color.green
-                val b = color.blue
-                print(b.toUByte().to8BitString() + " ")
-            }
-            println("")
-        }
-    }
-
-    fun encryptMsg(msg: String, buffImage: BufferedImage) {
-        var byteArr = msg.encodeToByteArray()
-        byteArr += byteArrayOf(0, 0, 3) // Adding the anchor
-
+    private fun encryptMsg(msg: ByteArray, buffImage: BufferedImage) {
+        val anchoredMSG = msg + byteArrayOf(0, 0, 3) // Adding the anchor
         var index = 0
-        val flatStrBitArr = byteArr.map { it.to8BitString() }.joinToString("") // This Array holds the msg in 8 bit as a flat String
+        val flatStrBitArr =
+            anchoredMSG.joinToString("") { it.to8BitString() } // This Array holds the msg in 8 bit as a flat String
         loop@ for (y in 0 until buffImage.height) {
             for (x in 0 until buffImage.width) {
-                if (index == byteArr.size * 8) break@loop // breaks the loop when all elements from the array are set
+                if (index == anchoredMSG.size * 8) break@loop // breaks the loop when all elements from the array are set
                 val color = Color(buffImage.getRGB(x, y)) // reading current rgb values for coordinate x and y
                 val r = color.red
                 val g = color.green
-                var b = color.blue
+                val b = color.blue
                 var bBitStr = b.toUByte().to8BitString()
                 bBitStr = bBitStr.substring(0, bBitStr.lastIndex) + flatStrBitArr[index]
                 val newColor = Color(r, g, bBitStr.toInt(2))
@@ -112,7 +100,7 @@ class ImageController {
         }
     }
 
-    fun decryptMsg() : String{
+    private fun decryptMsg(): String {
         println("Input image file:")
         val input = readln()
         val buffImage = readImage(File(input)) ?: return ""
@@ -120,7 +108,7 @@ class ImageController {
         val anchor = mutableListOf("00000000", "00000000", "00000011")
         val queue = MutableList(3) { "" }
         val tempCharList = mutableListOf<Char>()
-        loop@for (y in 0 until buffImage.height) {
+        loop@ for (y in 0 until buffImage.height) {
             for (x in 0 until buffImage.width) {
                 if (anchor.toTypedArray().contentDeepEquals(queue.toTypedArray())) break@loop
                 val color = Color(buffImage.getRGB(x, y))
@@ -136,18 +124,12 @@ class ImageController {
                 }
             }
         }
-        return secretMsg.apply { removeAll(anchor) }.map { it.toByte(2)}.toByteArray().decodeToString()
+        val password = requestPW()
+        val msgArr = secretMsg.run { dropLast(3) }.map { it.toByte(2) }.toByteArray().decodeToString()
 
-    }
+        val decryptedMsg = pwdEncryptMsg(msgArr, password)
+        return decryptedMsg.decodeToString()
 
-    fun Byte.to8BitString(): String {
-        val strRep = this.toString(2)
-        return "0".repeat(8 - strRep.length) + strRep
-    }
-
-    fun UByte.to8BitString(): String {
-        val strRep = this.toString(2)
-        return "0".repeat(8 - strRep.length) + strRep
     }
 
     /**
@@ -155,19 +137,54 @@ class ImageController {
      * the encrypted message will be output in the set directory
      */
     fun hideMessage() {
-        val configList = getImageConfig()
-        val bImage = readImage(configList.first()) ?: return
-        val bImageConverted = BufferedImage(bImage.width, bImage.height, BufferedImage.TYPE_INT_RGB) // Converting bufferedImage bImage to correct Type
-        bImageConverted.run { createGraphics().drawImage(bImage, 0, 0, null) }
+        val pathList = getIOFilePaths()
+        val buffImage = readImage(pathList.first()) ?: return
+        val buffImageConv = BufferedImage(
+            buffImage.width,
+            buffImage.height,
+            BufferedImage.TYPE_INT_RGB
+        ) // Converting bufferedImage bImage to correct Type
+        buffImageConv.run { createGraphics().drawImage(buffImage, 0, 0, null) }
 
         secretMsg = getSecretMessage()
-        if (secretMsg.length * 8 > bImageConverted.width * bImageConverted.height) {
+        val password = requestPW()
+        if (secretMsg.length * 8 > buffImageConv.width * buffImageConv.height) {
             println("The input image is not large enough to hold this message.")
             return
         }
-        encryptMsg(secretMsg, bImageConverted)
-        saveImageInPNG(bImageConverted, configList.last())
+        val msgWithPW = pwdEncryptMsg(secretMsg, password)
+        encryptMsg(msgWithPW, buffImageConv)
+        saveImageInPNG(buffImageConv, pathList.last())
 
+    }
+
+    /**
+     * symmetric key function. encrypts and decrypts a string with a password string.
+     * if you use the password string on the result again, you will receive the original string back.
+     */
+    private fun pwdEncryptMsg(msg: String, pwd: String): ByteArray {
+
+        val secretByteArr = msg.encodeToByteArray()
+
+        var pwdByteArr = pwd.encodeToByteArray()
+        while (pwdByteArr.size < secretByteArr.size) {
+            pwdByteArr += if (2 * pwdByteArr.size < secretByteArr.size) {
+                pwdByteArr
+            } else {
+                pwdByteArr.copyOfRange(0, secretByteArr.size - pwdByteArr.size)
+            }
+        }
+        val encrWithXOR = mutableListOf<Byte>()
+        for (index in secretByteArr.indices) {
+            encrWithXOR.add((secretByteArr[index].toInt() xor pwdByteArr[index].toInt()).toByte())
+        }
+
+        return encrWithXOR.toByteArray()
+    }
+
+    private fun requestPW(): String {
+        println("Password:")
+        return readln()
     }
 
     fun showMessage() {
@@ -175,7 +192,18 @@ class ImageController {
         println("Message:")
         println(decrypted)
     }
+
+    private fun Byte.to8BitString(): String {
+        val strRep = this.toString(2)
+        return "0".repeat(8 - strRep.length) + strRep
+    }
+
+    private fun UByte.to8BitString(): String {
+        val strRep = this.toString(2)
+        return "0".repeat(8 - strRep.length) + strRep
+    }
 }
+
 
 class UI(val imageController: ImageController) {
 
@@ -195,6 +223,9 @@ class UI(val imageController: ImageController) {
     }
 
 }
+
+
+
 
 
 
